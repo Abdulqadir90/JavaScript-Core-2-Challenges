c2016793701bf182c5f2c48fb790536cfcc8627f
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-02-27 00:49:33 +0000

    fastest presser done

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,38 @@
-function startGame() {}
+let seconds = document.querySelector(".seconds");
+let letterS = document.querySelector(".letter-s");
+let letterL = document.querySelector(".letter-l");
+let result = document.querySelector(".result");
 
-function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
-    // On 'S' Pressed
-  } else if (e.keyCode === 76) {
-    // On 'L' Pressed
-  }
+function startGame() {
+  letterS.innerHTML = 0;
+  letterL.innerHTML = 0;
+  result.innerHTML = "";
+  document.addEventListener("keypress", keyBoardEvents);
+
+  setTimeout(() => {
+    document.removeEventListener("keypress", keyBoardEvents);
+    // letterS.innerHTML > letterL.innerHTML
+    //   ? (result.innerHTML = "S Winner")
+    //   : letterL.innerHTML > letterS.innerHTML
+    //   ? (result.innerHTML = "L winner")
+    //   : (result.innerHTML = "Draw try again");
+    if(Number(letterS.innerHTML) > Number(letterL.innerHTML)){
+       result.innerHTML = "S Winner";
+    } else if (Number(letterL.innerHTML) > Number(letterS.innerHTML)) {
+       result.innerHTML = 'L Winner';
+    } else {
+       result.innerHTML = 'Draw, try again';
+    }
+
+    
+  }, seconds.value * 1000);
 }
 
-document.addEventListener("keypress", keyBoardEvents);
+function keyBoardEvents(e) {
+  if (e.key === "s") {
+    // On 'S' Pressed
+    letterS.innerHTML++;
+  } else if (e.key === "l") {
+    letterL.innerHTML++; // On 'L' P ressed
+  }
+}
