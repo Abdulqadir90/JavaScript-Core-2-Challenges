c5dbdd24d6198135c481024902b3c7ca0f483ab9
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-03-02 18:22:02 +0000

    guess the number done

--- challenge-guess-the-number/script.js
+++ challenge-guess-the-number/script.js
@@ -1,11 +1,32 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 
+let numOfTries = document.querySelector(".Tries-output");
+let finalOutput = document.querySelector(".final-output");
+let reset = document.querySelector(".btnNewGame");
+let playerGuess = document.querySelector(".inputs-Values");
+let triesLimit = 3;
+let currentTries = 0;
+
+
+
+
+
 function guessNumber() {
   //Collect input from the user
-  let guess = document.querySelector(".inputs-Values").value;
+  console.log('player', playerGuess.value);
+  console.log('RandomNumber', randomNumber)
 
+  numOfTries.innerHTML = currentTries;
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
-
+  if (playerGuess.value === '' || playerGuess.value <= 0 || playerGuess.value > 100) {
+    finalOutput.innerHTML = 'Please enter a number between 1 and 10';
+  } else if (playerGuess.value > randomNumber) {
+    finalOutput.innerHTML = 'Your guess is too high';
+  } else if (playerGuess.value < randomNumber) {
+    finalOutput.innerHTML = ('You guessed too low');
+  } else {
+    finalOutput.innerHTML = ('Well done G');
+  }
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
 
   //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
@@ -20,16 +41,29 @@ function guessNumber() {
 function newGame() {
   //Your code here
   //Reset randomNumber
+  randomNumber = Math.floor(Math.random() * 100 + 1);
   //Reset users input field
+  playerGuess.value = '';
   //Reset tries, and triesTaken by the user
+  numOfTries.innerHTML = '';
+  finalOutput.innerHTML = 'Guess a number between 1 and 100';
+  //current tries
+  currentTries = 0;
 }
 
 //keyboard exception
 function keyBoardEvents(e) {
+  if (currentTries !== triesLimit) {
   if (e.keyCode === 13) {
+    currentTries++;
     guessNumber();
+  }
+}
+   else{
+    finalOutput.innerHTML = 'You ran out of goes bro';
   }
 }
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
 document.addEventListener("keypress", keyBoardEvents);
+document.querySelector('.btnNewGame').addEventListener('click', newGame);
